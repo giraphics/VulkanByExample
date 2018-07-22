77a6b000b9dd3615e771d06f4f3ed58ca3f818c8
Parminder <engineer.parminder@gmail.com>
2018-07-22 16:36:52 +0800

    Scene Graph Intro app added.

---
+++ source/Chapter6/Ch6_02_SceneGraph_Intro/TempEx2_SceneGraph/Node.cpp
@@ -0,0 +1,161 @@
+#include "Node.h"
+#include<QMouseEvent>
+#include<glm/gtx/string_cast.hpp>
+
+Node::Node(Scene* p_Scene, Node* p_Parent, const BoundingRegion& p_BoundedRegion, const QString& p_Name, SHAPE p_ShapeType)
+    : m_Scene(p_Scene)
+    , m_Parent(p_Parent)
+    , m_Name(p_Name)
+    , m_ShapeType(p_ShapeType)
+    , m_BoundedRegion(p_BoundedRegion)
+    , m_OriginOffset(glm::vec3(0.0f, 0.0f, 0.0f))
+{
+    m_Parent ? m_Parent->m_ChildList.append(this) : p_Scene->AddItem(this);
+    
+    // Todo: We can directly use the translate as the m_BoundedRegion is already set
+    SetGeometry(m_BoundedRegion.m_Position.x, m_BoundedRegion.m_Position.y, m_BoundedRegion.m_Dimension.x, m_BoundedRegion.m_Dimension.y, m_BoundedRegion.m_Position.z);
+}
+
+void Node::Setup()
+{
+    foreach(Node* childItem, m_ChildList)
+    {
+        if (!childItem) continue;
+
+        childItem->Setup();
+    }
+}
+
+Node* Node::GetParent() const
+{
+    return m_Parent;
+}
+
+void Node::ApplyTransformation()
+{
+    *m_Scene->Transform().GetModelMatrix() *= m_Model;
+}
+
+glm::mat4 Node::GetParentsTransformation(Node *p_Parent) const
+{
+    return p_Parent ? (GetParentsTransformation(p_Parent->GetParent()) * p_Parent->m_Model) : glm::mat4();
+}
+
+void Node::Update(Node* p_Item)
+{
+    m_Scene->PushMatrix();
+    if (p_Item)
+    {
+        m_Scene->ApplyTransformation(p_Item->GetParentsTransformation(GetParent()) * m_Model); // This retrives all the transformation from the parent
+    }
+    else
+    {
+        m_Scene->ApplyTransformation(m_Model);
+    }
+
+    m_AbsoluteTransformation = *m_Scene->GetRefTransform().GetModelMatrix();
+    
+    QList<Node*>& childList = (p_Item ? p_Item->m_ChildList : m_ChildList);
+    Q_FOREACH(Node* childItem, childList)
+    {
+        if (!childItem) continue;
+
+        childItem->Update();
+    }
+
+    m_Scene->PopMatrix();
+}
+
+void Node::Rotate(float p_Angle, float p_X, float p_Y, float p_Z)
+{
+    if (m_OriginOffset != glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        m_Model = glm::translate(m_Model, m_OriginOffset);
+    }
+
+    m_Model = glm::rotate(m_Model, p_Angle, glm::vec3(p_X, p_Y, p_Z));
+
+    if (m_OriginOffset != glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        m_Model = glm::translate(m_Model, -m_OriginOffset);
+    }
+}
+
+void Node::Translate(float p_X, float p_Y, float p_Z)
+{
+    if (m_OriginOffset != glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        m_Model = glm::translate(m_Model, m_OriginOffset);
+    }
+
+    m_Model = glm::translate(m_Model, glm::vec3(p_X, p_Y, p_Z));
+
+    if (m_OriginOffset != glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        m_Model = glm::translate(m_Model, -m_OriginOffset);
+    }
+}
+
+void Node::Scale(float p_X, float p_Y, float p_Z)
+{
+    if (m_OriginOffset != glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        m_Model = glm::translate(m_Model, m_OriginOffset);
+    }
+
+    m_Model = glm::scale(m_Model, glm::vec3(p_X, p_Y, p_Z));
+
+    if (m_OriginOffset != glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        m_Model = glm::translate(m_Model, -m_OriginOffset);
+    }
+}
+
+void Node::SetGeometry(float p_X, float p_Y, float p_Width, float p_Height, float p_ZOrder/*=0*/)
+{
+    Translate(p_X, p_Y, p_ZOrder);
+
+    m_BoundedRegion.m_Position.x = p_X;
+    m_BoundedRegion.m_Position.y = p_Y;
+    m_BoundedRegion.m_Position.z = p_ZOrder;
+
+    m_BoundedRegion.m_Dimension.x = p_Width;
+    m_BoundedRegion.m_Dimension.y = p_Height;
+}
+
+void Node::SetZOrder(float p_ZOrder)
+{
+    m_BoundedRegion.m_Position.z = p_ZOrder;
+
+    Reset();
+    Translate(m_BoundedRegion.m_Position.x, m_BoundedRegion.m_Position.y, m_BoundedRegion.m_Position.z);
+}
+
+void Node::SetPosition(float p_X, float p_Y)
+{
+    glm::vec4 posStart((0 * m_BoundedRegion.m_Dimension.x), (0 * m_BoundedRegion.m_Dimension.y), 0.0, 1.0);
+    glm::vec4 posStartResult = m_AbsoluteTransformation * posStart;
+
+    glm::vec4 posEnd((m_BoundedRegion.m_Dimension.x), (m_BoundedRegion.m_Dimension.y), 0.0, 1.0);
+    glm::vec4 posEndResult = m_AbsoluteTransformation * posEnd;
+
+    m_BoundedRegion.m_Position.x = p_X;
+    m_BoundedRegion.m_Position.y = p_Y;
+
+    Reset();
+    Translate(m_BoundedRegion.m_Position.x, m_BoundedRegion.m_Position.y, m_BoundedRegion.m_Position.z);
+    m_AbsoluteTransformation = m_Model * GetParentsTransformation(GetParent());
+}
+
+void Node::GatherFlatNodeList()
+{
+    if (!m_Scene) return;
+
+    m_Scene->AppendToFlatNodeList(this);
+
+    Q_FOREACH(Node* childItem, m_ChildList)
+    {
+        assert(childItem);
+        childItem->GatherFlatNodeList();
+    }
+}
---
+++ source/Chapter6/Ch6_02_SceneGraph_Intro/TempEx2_SceneGraph/Node.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "Scene.h"
+#include "../common/VulkanHelper.h"
+
+class VulkanApp;
+
+class RenderSchemeFactory
+{
+public:
+    RenderSchemeFactory() {}
+    virtual ~RenderSchemeFactory() {}
+
+    virtual void Setup() {}
+    virtual void Update() {}
+    virtual void Render(VkCommandBuffer& p_CommandBuffer) {}
+    virtual void Prepare(Scene* p_Scene) {}
+    virtual void UpdateNodeList(Node* p_Parent) {}
+    virtual void ResizeWindow(VkCommandBuffer& p_CommandBuffer) {}
+
+    GETSET(glm::mat4x4, ProjectViewMatrix);
+    GETSET(VulkanApp*, VulkanApplication)
+
+protected:
+    QMap<QString, QPair<VkPipeline, VkPipelineLayout> > m_GraphicsPipelineMap;
+};
+
+class Node
+{
+public:
+    Node(Scene* p_Scene, Node* p_Parent, const BoundingRegion& p_BoundedRegion, const QString& p_Name = "", SHAPE p_ShapeType = SHAPE::SHAPE_NONE);
+
+    virtual void Setup();
+    virtual void Update(Node* p_Item = NULL);
+    virtual RenderSchemeFactory* GetRenderSchemeFactory() { return NULL; } // Custom model class do not need to implement it as they are made of basic model classes.
+
+    void Rotate(float p_Angle, float p_X, float p_Y, float p_Z);
+    void Translate(float p_X, float p_Y, float p_Z);
+    void Scale(float p_X, float p_Y, float p_Z);
+
+    void Reset() { m_Model = glm::mat4(); }
+    void SetZOrder(float p_ZOrder);
+    void SetPosition(float p_X, float p_Y);
+    void SetGeometry(float p_X, float p_Y, float p_Width, float p_Height, float p_ZOrder = 0.0f);
+
+    inline Node* GetParent() const;
+    void ApplyTransformation();
+    glm::mat4 GetParentsTransformation(Node* p_Parent) const;
+    void GatherFlatNodeList();
+
+    virtual void ResizeWindow(int width, int height) {}
+
+    GETSET(QString,                     Name)
+    GETSET(SHAPE,                       ShapeType);
+    GETSET(BoundingRegion,              BoundedRegion)
+    GETSET(glm::vec4,                   Color)
+    GETSET(glm::mat4,                   Model)
+    GETSET(glm::vec3,                   OriginOffset)
+    GETSET(glm::mat4,                   AbsoluteTransformation)
+    GETSET(Scene*,                      Scene)
+    GETSET(Node*,                       Parent)
+    GETSET(QList<Node*>,                ChildList)
+};
